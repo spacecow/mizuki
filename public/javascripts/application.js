7a5a42cee6ae872b13f891893c7b0e2aea5bc9e5
Johan Sveholm <jsveholm@gmail.com>
2011-04-22 17:16:09 +0900

    stars for the start

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,8 @@
 $(function() {
-  $("div#star1").mouseover(function() {
-    sparkle1 = setInterval(hoverfade1, 5500);
-    sparkle1 = setInterval(hoverfade2, 3500);
-    sparkle1 = setInterval(hoverfade3, 8000);
-    sparkle1 = setInterval(hoverfade4, 12000);
-  });
+  sparkle1 = setInterval(hoverfade1, 5500);
+  sparkle2 = setInterval(hoverfade2, 3500);
+  sparkle3 = setInterval(hoverfade3, 8000);
+  sparkle4 = setInterval(hoverfade4, 12000);
   function hoverfade1(){ $('div#star1').fadeOut(1000).fadeIn(1000); }
   function hoverfade2(){ $('div#star2').fadeOut(1500).fadeIn(1500); }
   function hoverfade3(){ $('div#star3').fadeOut(1200).fadeIn(1200); }
