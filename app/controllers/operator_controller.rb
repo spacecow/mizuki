97359614d8b088cfd07e0468856e305e1cd72a25
Johan Sveholm <jsveholm@gmail.com>
2011-04-26 09:41:05 +0900

    god mode at welcome page

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,9 +1,11 @@
 class OperatorController < ApplicationController
   def welcome
-    prev = Maintenance.where(:ip => request.remote_ip)
-    m = Maintenance.new(:ip => request.remote_ip, :agent => request.user_agent)
-    m.place = prev.first.place unless prev.empty?
-    m.save
+    unless god?
+      prev = Maintenance.where(:ip => request.remote_ip)
+      m = Maintenance.new(:ip => request.remote_ip, :agent => request.user_agent)
+      m.place = prev.first.place unless prev.empty?
+      m.save
+    end
   end
 
 end
