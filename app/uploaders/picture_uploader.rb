621b22c9b4405850344a73effb93a64d49363027
Johan Sveholm <jsveholm@gmail.com>
2011-04-29 10:44:51 +0900

    adjustment to galleriffic

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,6 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     ENV['app_root'] + "/tmp"
+
     #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
