52e4dbcf9e99ebb9dc12c99547385f84e8f75a51
Johan Sveholm <jsveholm@gmail.com>
2011-05-04 00:58:35 +0900

    datepicker and timepicker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,4 +21,9 @@ $(function() {
   function showEvent($id) {
     $.getScript($id);
   }
+
+  $("#event_start_date").datepicker({dateFormat: 'yy-mm-dd'});
+  $("#event_end_date").datepicker({dateFormat: 'yy-mm-dd'});
+  $("#event_start_time").timepicker();
+  $("#event_end_time").timepicker();
 });
