1794134e2f56a9bd80ae1ee7c0b495021025785c
Johan Sveholm <jsveholm@gmail.com>
2011-06-16 16:42:42 +0900

    s3 configuration

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,9 +18,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   #end
 
-  def cache_dir
-    "#{RAILS_ROOT}/tmp/uploads"
-  end
+  #def cache_dir
+  #  "#{RAILS_ROOT}/tmp/uploads"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
