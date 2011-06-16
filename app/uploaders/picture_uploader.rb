93d3321902fb7ecd3c9fe24ea3c80355a7289235
Johan Sveholm <jsveholm@gmail.com>
2011-06-16 16:33:18 +0900

    iphone 480

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,6 +18,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   #end
 
+  def cache_dir
+    "#{RAILS_ROOT}/tmp/uploads"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
